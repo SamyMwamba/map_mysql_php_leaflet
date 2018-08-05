319e50003c1b0efe0793ff3fb7cf649337e72fc2
samymwamba <samymwamba@gmail.com>
2018-08-05 16:10:43 +0200

    setting lubumbashi gps

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
   <body>
 
     <body>
-      <div id="map" style="width: 600px; height: 400px"></div>
+      <div id="map" style="width: 100%; height: 550px"></div>
           <script>
-           var map = L.map('map').setView([27.4837274,-11.66494], 13);
+           var map = L.map('map').setView([-11.66494,27.4837274], 13);
            L.tileLayer( 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2FteW13YW1iYSIsImEiOiJjamtnd2FlbDE1M2l5M3dtbGY1Z2wzbzJjIn0.7dqO-EqSJpSyxyUniLkUNQ', {
            maxZoom: 18,
            attribution: 'Map data &copy; <a href="http://openstreetmap.org/"> OpenStreetMap </a> contributors, ' +
